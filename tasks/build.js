c27dd43f01e1dfccab20c6a3ee370c801099937c
chen roth <rothchen@gmail.com>
2016-02-20 05:09:49 +0200

    switch to gulp-markdown

--- tasks/build.js
+++ tasks/build.js
@@ -1,12 +1,15 @@
 'use strict';
 const nunjucks = require('gulp-nunjucks');
-const marked = require('gulp-marked-mustache');
+const gulpMarked = require('gulp-marked-mustache');
+const markdown = require('gulp-markdown');
+const frontmatter = require('gulp-front-matter');
 
 let BUILD_PATH = './build/';
 
 module.exports = (gulp, metadata) => {
   gulp.task('build', ['metadata', 'clean'], () => {
     gulp.src('content/**/*.md')
+      .pipe(frontmatter())
       .pipe(interpolate(metadata))
       .pipe(md2html())
       .pipe(gulp.dest(BUILD_PATH));
@@ -19,6 +22,7 @@ function interpolate(metadata) {
    * @param  {object} metadata data to interpolate
    * @return {Stream.Writable}          a writable stream to gulp.pipe to
    */
+
   return nunjucks.compile(metadata.fields);
 }
 
@@ -27,7 +31,19 @@ function md2html() {
    * the implementation detail of converting md files to html
    * @return {Stream.Writable}   a writable stream to gulp.pipe to
    */
-  return marked({
-    templatePath: './templates/'
+
+  let renderer = new markdown.marked.Renderer();
+  renderer.table = function(header, body) {
+    return `<div class="table"> \
+              <table> \
+                <thead>${header}</thead> \
+                <tbody>${body}</tbody> \
+              </table>\
+            </div>`;
+  }
+  return markdown({
+    //templatePath: './templates/',
+    renderer: renderer
+
   });
 }
