d8d829df3440fefc5c2a2b582f350838fe1c4e58
chen roth <rothchen@gmail.com>
2016-02-27 00:23:37 +0200

    change default versions source to cloudify-manager

--- tasks/metadata/index.js
+++ tasks/metadata/index.js
@@ -4,7 +4,7 @@ const getVersions = require('./get-versions');
 module.exports = (gulp, metadata) => {
   gulp.task('metadata', () => {
     verbose('preparing metadata...');
-    metadata.prepare('versions', getVersions, 'https://github.com/cloudify-cosmo/docs.getcloudify.org.git');
+    metadata.prepare('versions', getVersions, 'https://github.com/cloudify-cosmo/cloudify-manager.git');
     metadata.prepare('env', process.env['NODE_ENV'] || 'development');
     return metadata.ready((fields) => {
       verbose(metadata);
